be2a58dc336d19b8cd8a768674ea15da17bb3a67
GeraCode <gsalvador.bermudez@gmail.com>
2025-07-30 20:44:21 -0600

    Âfeat: first commit

---
+++ src/main/java/com/geracode/java_knowledge_roadmap/JavaKnowledgeRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.geracode.java_knowledge_roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKnowledgeRoadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKnowledgeRoadmapApplication.class, args);
+	}
+
+}
